e4af9af2c967f3dbba5f3525cfb3f1543b24a102
Breno Ryoti <brenopsilva95@hotmail.com>
2023-12-01 20:58:07 -0300

    correcao de erro na solicitacao

--- src/BoletoSimples/ResponseError.php
+++ src/BoletoSimples/ResponseError.php
@@ -18,9 +18,21 @@ class ResponseError extends \Exception
     {
         $this->response = $response;
 
-        $json = $response->json();
-        if (isset($json['error'])) {
-            $this->message = $json['error'];
+        $json = json_decode($response->getBody()->getContents(), true);
+
+        if (isset($json['errors'])) {
+            $message = '';
+            if (isset($json['errors'][0]['title'])) {
+                $message .= $json['errors'][0]['title'] . ' - ';
+            }
+            if (isset($json['errors'][0]['code'])) {
+                $message .= $json['errors'][0]['code'] . ' - ';
+            }
+
+            if (isset($json['errors'][0]['detail'])) {
+                $message .= $json['errors'][0]['detail'] . ' - ';
+            }
+            $this->message = $message;
 
             throw $this;
         }
