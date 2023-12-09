2cc864770fefe823ebf69b561f98fa98e8ef221e
Breno Ryoti <brenopsilva95@hotmail.com>
2023-12-08 21:16:33 -0300

    adicionando endpoint para homologar carteira

--- src/BoletoSimples/Resources/BankBilletAccount.php
+++ src/BoletoSimples/Resources/BankBilletAccount.php
@@ -4,4 +4,13 @@ namespace BoletoSimples;
 
 class BankBilletAccount extends BaseResource
 {
+    public static function ask($bankBilletId)
+    {
+        if (!$bankBilletId) {
+            throw new \Exception("Couldn't find ".get_called_class().' without an id for wallet.');
+        }
+        $response = self::sendRequest('GET', "bank_billet_accounts/{$bankBilletId}/ask");
+
+        return new BankBilletAccount($response->json());
+    }
 }
