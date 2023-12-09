829778643fe1d2bb1ec48abf277cb7545aa36554
Breno Ryoti <brenopsilva95@hotmail.com>
2023-12-08 21:50:46 -0300

    correcao homologacao de conta

--- src/BoletoSimples/Resources/BankBilletAccount.php
+++ src/BoletoSimples/Resources/BankBilletAccount.php
@@ -9,9 +9,8 @@ class BankBilletAccount extends BaseResource
         if (!$bankBilletId) {
             throw new \Exception("Couldn't find ".get_called_class().' without an id for wallet.');
         }
-        $response = self::sendRequest('GET', "bank_billet_accounts/{$bankBilletId}/ask");
+        self::sendRequest('GET', "bank_billet_accounts/{$bankBilletId}/ask");
 
-        return new BankBilletAccount($response->json());
     }
 
     public static function delete($bankBilletId)
